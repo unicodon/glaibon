58c5882ebd2b87b67cd1441ead96a6f62a64c7bf
U-user-PC\user <user@user-PC.(none)>
2014-01-29 00:43:36 +0900

    Added model data json.

---
+++ model/js/body02.js
@@ -0,0 +1,57 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.65 Exporter",
+		"vertices"      : 4,
+		"faces"         : 2,
+		"normals"       : 3,
+		"colors"        : 0,
+		"uvs"           : [3],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "metalA",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.17160001482963594, 0.17160001482963594, 0.17160001482963594],
+		"colorDiffuse" : [0.28600001335144043, 0.28600001335144043, 0.28600001335144043],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 5,
+		"transparency" : 1.0,
+		"transparent" : true,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [0.0174,-0.00419999,0.1141,0,0.00250001,0.1153,0,-0.00999999,0.1133,-0.0174,-0.00419999,0.1141],
+
+	"morphTargets" : [],
+
+	"normals" : [0,-0.157964,0.987426,0.007233,-0.157964,0.987396,-0.007233,-0.157964,0.987396],
+
+	"colors" : [],
+
+	"uvs" : [[0,0,1,0,1,1]],
+
+	"faces" : [42,2,0,1,0,0,1,2,0,1,0,42,1,3,2,0,0,1,2,0,2,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animations" : []
+
+
+}
